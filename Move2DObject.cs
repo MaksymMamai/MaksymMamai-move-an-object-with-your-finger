eac6d430ed07d71ef1d181c1d725f310412e7aa5
MaksymMama <167803652+MaksymMama@users.noreply.github.com>
2025-03-17 15:53:18 +0200

    Add enother one issue

--- Move2DObject.cs
+++ Move2DObject.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class Move2DObject : MonoBehaviour
 {
     [SerializeField] private GameObject square;
-
+    private Touch touch;
+    private float speedModifire;
     private float positionX, positionY;
 
     private void Update()
     {
-        MoveWithFinger();
+
     }
 
     private void MoveWithFinger()
@@ -28,12 +29,26 @@ public class Move2DObject : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
-            Touch touch = Input.GetTouch(0); 
+            touch = Input.GetTouch(0);
 
             if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
             {
                 Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
                 transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime * 4);
+            }
+        }
+    }
+
+    private void MoveWithFinger3DSecondVercion()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * speedModifire,
+                                                 transform.position.y + touch.deltaPosition.y * speedModifire,
+                                                 transform.position.z);
             }
         }
     }
